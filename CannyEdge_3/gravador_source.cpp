60967bfdad76dcdacf700a6e9e1d388b3dea6582
Backfire77 <guilherme.moreira@tecnico.ulisboa.pt>
2017-02-24 19:49:01 +0000

    A funcionar o retirar frames com o canny

--- CannyEdge_3/gravador_source.cpp
+++ CannyEdge_3/gravador_source.cpp
@@ -16,41 +16,39 @@ int main(int argc, char** argv)
 	}
 
 
-
-
 	double count = cap.get(CV_CAP_PROP_FRAME_COUNT); //get the frame count 
 
 	double Pos = 1; // evitar o primeiro frame
 	cout << count << endl;
 	cap.set(CV_CAP_PROP_POS_FRAMES, Pos); //Set index to first frame (simbolo da playstation)
-	namedWindow("MyVideo", CV_WINDOW_AUTOSIZE);
+	/*namedWindow("MyVideo", CV_WINDOW_AUTOSIZE);
 	namedWindow("Teste_Gray", CV_WINDOW_AUTOSIZE);
 	namedWindow("Teste_GaussianBlur", CV_WINDOW_AUTOSIZE);
 	namedWindow("Teste_Canny", CV_WINDOW_AUTOSIZE);
-	
+	*/
 	while (Pos <= count)
 	{
-
+		cap.set(CV_CAP_PROP_POS_FRAMES, Pos); //Set index to first frame (simbolo da playstation)
 		Mat frame;
 		bool success = cap.read(frame);
 		if (!success) {
 			cout << "Cannot read  frame " << endl;
 			break;
 		}
-		imshow("MyVideo", frame);
+		//imshow("MyVideo", frame);
 
 		// parte de tu a tentar um grayscale
 		Mat frame_gray;
 		cvtColor(frame, frame_gray, CV_BGR2GRAY);
-		imshow("Teste_Gray", frame_gray);
+	//	imshow("Teste_Gray", frame_gray);
 		//GaussianBlur
 		Mat frame_gaussian;
 		GaussianBlur(frame_gray, frame_gaussian, Size(7, 7), 1.5, 1.5);
-		imshow("Teste_GaussianBlur", frame_gaussian);
+	//	imshow("Teste_GaussianBlur", frame_gaussian);
 		//Canny edge
 		Mat frame_canny;
 		Canny(frame_gaussian, frame_canny, 0, 30, 3);
-		imshow("Teste_Canny", frame_canny);
+	//	imshow("Teste_Canny", frame_canny);
 
 
 		// http://docs.opencv.org/2.4/doc/tutorials/introduction/load_save_image/load_save_image.html
@@ -59,8 +57,8 @@ int main(int argc, char** argv)
 
 		string varAsString = to_string(Pos_int);
 
-		String directory = "../../data/Output/Canny_Image_a" + varAsString +".jpg";
-		cout << directory;
+		String directory = "../../data/Output/Canny_Image_Teste" + varAsString +".jpg";
+		//cout << directory;
 
 
 		imwrite(directory, frame_canny);
@@ -68,7 +66,12 @@ int main(int argc, char** argv)
 		//	cvSaveImage("frame/canny.jpg", img);
 
 		if (waitKey(0) == 27) break;
+		
+		Pos=Pos+10; // para não fazer todos os frames
 
+		if (Pos_int % 10 == 0) {
+			cout << "Mais 100" << endl;
+		}
 	}
-
+	cout << "Feito" << endl;
 }
